d47d4d68e4f2d30bf38f7cd3b21c26435a750a29
Tanush Pandya <tanush.pandya@gmail.com>
2025-03-21 18:06:10 -0400

    fixed env again

--- coop-react/src/firebaseConfig.jsx
+++ coop-react/src/firebaseConfig.jsx
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.VITE_API_FIREBASE,
+  apiKey: import.meta.env.VITE_API_FIREBASE,
   authDomain: "scholar-save.firebaseapp.com",
   projectId: "scholar-save",
   storageBucket: "scholar-save.firebasestorage.app",
