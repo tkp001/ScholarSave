1518d60c039bc94f364f6e6ea5b1a13c21b0ff32
Tanush Pandya <tanush.pandya@gmail.com>
2025-04-04 14:43:35 -0400

    working on settings page and functionality

--- coop-react/src/pages/SettingsPage.jsx
+++ coop-react/src/pages/SettingsPage.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
+import { getAuth, sendEmailVerification } from "firebase/auth";
 import { SiSemanticscholar } from "react-icons/si";
 
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import UserContext from '../UserContext'
 
 
 const SettingsPage = () => {
+  const auth = getAuth();
+  const [verificationEmailSent, setVerificationEmailSent] = useState(false)
   const { user } = useContext(UserContext)
   
+  function sendUserEmailVerification() {
+    sendEmailVerification(auth.currentUser)
+    verificationEmailSent(true)
+    alert("Verification email sent to " + user.email)
+  }
+
   return (
     <>
-      <div className='flex flex-grow flex-nowrap overflow-y-auto no-scrollbar bg-gray-800 text-white'>
+      <div className='flex flex-grow flex- overflow-y- no-scrollbar bg-gray-800 text-white'>
         <div className="flex flex-col w-full p-10">
           <div className='flex flex-row'>
             <div className='flex flex-col w-100 min-w-100'>
@@ -21,21 +30,22 @@ const SettingsPage = () => {
 
               <div className='text-4xl my-4'>Account & Safety</div>
               <div className='text-md mb-5'>{`Email: ${user.email}`}</div>
-              <div className='flex flex-col justify-center bg-gray-700 w-fit p-2 h-8 rounded-xl my-1' onClick={console.log("click")}>Change Email</div>
-              <div className='flex flex-col justify-center bg-gray-700 w-fit p-2 h-8 rounded-xl my-1' onClick={console.log("click")}>Change Password</div>
-              <div className='flex flex-col justify-center bg-gray-700 w-fit p-2 h-8 rounded-xl my-1' onClick={console.log("click")}>Logout</div>
+              {!user.emailVerified && !verificationEmailSent ? <div className='flex flex-col justify-center bg-yellow-500 w-fit p-2 h-8 rounded-xl my-1' onClick={sendUserEmailVerification}>Verify Email</div> : null}
+              <div className='flex flex-col justify-center bg-gray-700 w-fit p-2 h-8 rounded-xl my-1' onClick={() => console.log("click")}>Change Email</div>
+              <div className='flex flex-col justify-center bg-gray-700 w-fit p-2 h-8 rounded-xl my-1' onClick={() => console.log("click")}>Reset Password</div>
+              <div className='flex flex-col justify-center bg-gray-700 w-fit p-2 h-8 rounded-xl my-1' onClick={() => console.log("click")}>Logout</div>
               
-              <div className='flex flex-col justify-center bg-red-700 w-fit p-2 h-8 rounded-xl my-1' onClick={console.log("click")}>Delete Account</div>
-              <div className='flex flex-col justify-center bg-red-700 w-fit p-2 h-8 rounded-xl my-1' onClick={console.log("click")}>Delete Data</div>
+              <div className='flex flex-col justify-center bg-red-700 w-fit p-2 h-8 rounded-xl my-1' onClick={() => console.log("click")}>Delete Account</div>
+              <div className='flex flex-col justify-center bg-red-700 w-fit p-2 h-8 rounded-xl my-1' onClick={() => console.log("click")}>Delete Data</div>
 
               <div className='text-4xl mt-10 my-4'>Customization</div>
               <div className='flex flex-row items-center my-1'>
                 <div className='text-2xl w-70'>Night Mode</div>
-                <input type="checkbox" className="" checked={true} onChange={console.log("click")}/>
+                <input type="checkbox" className="" checked={true} onChange={() => console.log("click")}/>
               </div>
               <div className='flex flex-row items-center my-1'>
                 <div className='text-2xl w-70'>Budget Warnings</div>
-                <input type="checkbox" className="" checked={true} onChange={console.log("click")}/>
+                <input type="checkbox" className="" checked={true} onChange={() =>  console.log("click")}/>
               </div>
               
             </div>
