a1b0b386540db8ad284124fe1f07fb364e9a1ca7
Tanush Pandya <tanush.pandya@gmail.com>
2025-05-12 17:02:52 -0400

    Update vite.config.js

--- coop-react/vite.config.js
+++ coop-react/vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import typography from '@tailwindcss/typography';
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    typography(),
   ],
 });
